b7c0b9a45148e62b995c73b7922f0e415713cae9
boithanh <boithanh01694@gmail.com>
2025-03-29 10:05:05 +0700

    fix build app

--- src/app/danh-muc-san-pham/page.tsx
+++ src/app/danh-muc-san-pham/page.tsx
@@ -4,8 +4,8 @@ interface Props {
     params: { slug: string };
 }
 
-const page = ({ params }: Props) => {
+const Page = ({ params }: Props) => {
     return <div>Danh mục sản phẩm: {params.slug}</div>;
 };
 
-export default page
+export default Page
