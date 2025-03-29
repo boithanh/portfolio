22a2c9111368722b44a730bf6b227c6b4dc1e7b6
boithanh <boithanh01694@gmail.com>
2025-03-29 10:00:42 +0700

    Fix for build vercel

--- src/app/danh-muc-san-pham/page.tsx
+++ src/app/danh-muc-san-pham/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-type Props = {}
-
-const page = (props: Props) => {
-    return (
-        <div>Danh mục sản phẩm</div>
-    )
+interface Props {
+    params: { slug: string };
 }
+
+const page = ({ params }: Props) => {
+    return <div>Danh mục sản phẩm: {params.slug}</div>;
+};
 
 export default page
