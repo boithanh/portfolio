77a6163c1710cfe7bc2eb79f8058e478a529bbf6
boithanh <boithanh01694@gmail.com>
2025-03-29 10:35:38 +0700

    fix

--- src/app/danh-muc-san-pham/page.tsx
+++ src/app/danh-muc-san-pham/page.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { notFound } from "next/navigation";
 
 interface Props {
     params: { slug: string };
 }
 
-const Page = ({ params }: Props) => {
+export async function generateStaticParams() {
+    const posts = await fetch("https://.../posts").then((res) => res.json());
+
+    return posts.map((post: any) => ({
+        slug: post.slug,
+    }));
+}
+
+const Page = async ({ params }: Props) => {
+    if (!params.slug) return notFound(); // Xử lý nếu slug không tồn tại
+
     return <div>Danh mục sản phẩm: {params.slug}</div>;
 };
 
