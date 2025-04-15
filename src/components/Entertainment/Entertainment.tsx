663140b842c58bcaf23edd4dcac298f27e4dafc4
boithanh <boithanh01694@gmail.com>
2025-04-15 12:16:42 +0700

    Insert Icon for IOS,Andoid and other

--- src/components/Entertainment/Entertainment.tsx
+++ src/components/Entertainment/Entertainment.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Entertainment = (): React.ReactNode => {
     return (
-        <section className='entertainment'>
+        <section className='entertainment user-select-none'>
             <div className='enter-inner'>
                 <div className='title'>
                     <h2 className='text-end mb-4 px-3'>Relax a bit <br /> before watching the next one.</h2>
                 </div>
                 <div className=" col-12 col-xl-6 mx-auto">
                     <div>
-                        <iframe width="100%" height={450} allowFullScreen={true} src="https://nebezb.com/floppybird/" className='user-select-none'></iframe>
+                        <iframe width="100%" height={450} allowFullScreen={false} allow="autoplay" src="https://nebezb.com/floppybird/" className='user-select-none'></iframe>
                     </div>
                 </div>
             </div>
