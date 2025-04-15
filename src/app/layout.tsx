785ce717ef2738ddd93503732effddc940db998a
Bối Thạnh <53029041+boithanh@users.noreply.github.com>
2025-04-15 11:31:01 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,16 +8,15 @@ import SupportProvider from "@/redux/SupportProvider";
 export const metadata: Metadata = {
   metadataBase: new URL("https://boithanh-dev.vercel.app"),
   title: "Bối Thạnh's Portfolio",
-  description: "This is my portfolio, thanks you for watching",
+  description: "Frontend Developer & UI/UX Enthusiast",
   openGraph: {
     url: "https://boithanh-dev.vercel.app/",
-    siteName: "My Portfolio",
+    siteName: "Thạnh's Portfolio",
     images: [
       {
         url: "https://boithanh-dev.vercel.app/images/seo.jpg",
         width: 1200,
         height: 630,
-        alt: "Preview of Bối Thạnh Portfolio",
       },
     ],
     locale: "vi_VN",
@@ -33,13 +32,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta property="og:title" content="Bối Thạnh's Portfolio" />
-        <meta property="og:description" content="Thank you for watching" />
-        <meta property="og:image" content="https://boithanh-dev.vercel.app/images/seo.jpg" />
-        <meta property="og:url" content="https://boithanh-dev.vercel.app" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Bối Thạnh's Portfolio" />
-        <meta name="twitter:description" content="Thank you for watching" />
+        <meta name="twitter:description" content="Frontend Developer & UI/UX Enthusiast" />
         <meta name="twitter:image" content="https://boithanh-dev.vercel.app/images/seo.jpg" />
         <link
           rel="stylesheet"
