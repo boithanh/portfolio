76e7460e696ff107dd81b37252b1c76368567389
boithanh <boithanh01694@gmail.com>
2025-04-18 16:20:39 +0700

    add animation for block CTA

--- src/components/CTA/CTA.tsx
+++ src/components/CTA/CTA.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import Link from 'next/link';
+import React, { useState } from 'react'
 
 type Props = {}
 
 const CTA = (props: Props) => {
+    const [isRoll, setIsRoll] = useState(false);
+    const [isLighting, setIsLighting] = useState(false);
     return (
-        <section className='cta'>
+        <section className={`cta${isRoll ? ' roller' : ''}`} onMouseEnter={() => { setIsRoll(!isRoll); setIsLighting(!isLighting) }} onMouseLeave={() => { setIsRoll(false); setIsLighting(false) }} >
             <div className='container-fluid'>
                 <div className="row">
                     <div className="col">
                         <div className="cta-content mx-auto text-left">
-                            <button className='btn btn-light p-sm-3'>Contact Now</button>
+                            <Link href={"/contact"} className="btn btn-light p-sm-3">Contact Now</Link>
                         </div>
                     </div>
                 </div>
             </div>
+            <div className={`overlay${isLighting ? " lighting" : ""}`}></div>
         </section>
     )
 }
