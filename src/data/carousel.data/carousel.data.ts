26ea560f6b720215496cf5f959b38b6beea14cc6
Boi Thanh <boithanh01694@gmail.,com>
2026-01-03 21:09:05 +0700

    Add BG for Tet

--- src/data/carousel.data/carousel.data.ts
+++ src/data/carousel.data/carousel.data.ts
@@ -1,44 +1,44 @@
 export const carouselData: Record<string, string>[] = [
   {
-    background: "christmas2025/noel_3.jpg",
+    background: `newyear2026/tet_1.jpg`,
     avatar: "avatar/06-3.jpg",
     title: "Hi, I'm Thạnh",
     content:
       "A budding Frontend Developer with a strong passion for minimal design and user experience. I’m always learning and growing.",
   },
   {
-    background: "christmas2025/noel_8.jpg",
+    background: "newyear2026/tet_3.jpg",
     avatar: "avatar/02-3.jpg",
     title: "Design meets function",
     content: "Where creativity turns into user experience",
   },
   {
-    background: "christmas2025/noel_4.jpg",
+    background: "newyear2026/tet_4.jpg",
     avatar: "avatar/03-3.jpg",
     title: "Build. Launch. Evolve",
     content: "From MVP to full-scale products — we’re with you",
   },
   {
-    background: "christmas2025/noel_5.jpg",
+    background: "newyear2026/tet_2.jpg",
     avatar: "avatar/04-3.jpg",
     title: "Start small. Grow smart",
     content: " From idea to product — we make it real",
   },
   {
-    background: "christmas2025/noel_17.jpg",
+    background: "newyear2026/tet_5.jpg",
     avatar: "avatar/05-3.jpg",
     title: "Our goals. Our mission",
     content: "How we help other companies to grow",
   },
   {
-    background: "christmas2025/noel_12.jpg",
+    background: "newyear2026/tet_6.jpg",
     avatar: "avatar/01-3.jpg",
     title: "Meet the Creator",
     content:
       "A passionate developer dedicated to building elegant and functional digital experiences",
   },
   {
-    background: "christmas2025/noel_19.jpg",
+    background: "newyear2026/tet_7.jpg",
     avatar: "avatar/08-3.jpeg",
     title: "My Vision & Values",
     content:
